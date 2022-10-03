8fcbf86e3b99254092726c66fbcd7b0a75a38202
Jan Willhaus <mail@janwillhaus.de>
2022-10-03 13:19:00 +0200

    Update to new installer

--- installer.sh
+++ installer.sh
@@ -4,29 +4,13 @@ set -eux
 export PYTHONUNBUFFERED=1
 export PYTHONDONTWRITEBYTECODE=1
 
-# Custom handling for slim images that have no tool for downloading things by default
-if [ "$IMAGE_FLAVOR" = "-alpine" ]; then
-    apk add --no-cache rsync
-else
-    apt-get update
-    apt-get install -y --no-install-recommends wget rsync
-fi
+python -m venv "$POETRY_HOME"
 
-echo "Downloading archive"
-wget -q -O /tmp/poetry.tar.gz \
-    "https://github.com/python-poetry/poetry/releases/download/${POETRY_VERSION}/poetry-${POETRY_VERSION}-linux.tar.gz"
+"$POETRY_HOME/bin/pip" install wheel
+"$POETRY_HOME/bin/pip" install "poetry==$POETRY_VERSION"
 
-echo "Verifying checksum"
-[ "$POETRY_SHASUM  /tmp/poetry.tar.gz" = "$(sha256sum /tmp/poetry.tar.gz)" ] || (echo "Checksumming failed" && exit 1)
-
-echo "Extracting"
-mkdir -p "$POETRY_HOME/lib"
-tar -xzf /tmp/poetry.tar.gz -C "$POETRY_HOME/lib"
-
-echo "Marking executable, symlinking, disabling virtualenv creation"
-chmod +x "$POETRY_HOME/bin/poetry"
+echo "Symlinking, disabling virtualenv creation"
 ln -s "$POETRY_HOME/bin/poetry" "/usr/local/bin/poetry"
-rsync -a "$POETRY_HOME/lib/poetry/_vendor/py${IMAGE_VERSION}/" "$POETRY_HOME/lib/"
 
 poetry config virtualenvs.create false
 
@@ -34,15 +18,7 @@ echo "Cleanup"
 rm -rf "$POETRY_HOME/lib/poetry/_vendor" /tmp/poetry* /tmp/installer.sh
 find "$POETRY_HOME" -depth \
     \( \
-        \( -type d -a \( -name test -o -name tests -o -name idle_test \) \) \
-        -o \
-        \( -type f -a \( -name '*.pyc' -o -name '*.pyo' \) \) \
-    \) -exec rm -rf '{}' +; \
-
-# Custom handling for slim images to remove previous additions
-if [ "$IMAGE_FLAVOR" = "-alpine" ]; then
-    apk del --no-cache rsync
-else
-    apt-get purge -y --auto-remove wget rsync
-    rm -rf /var/lib/apt/lists/*
-fi
+    \( -type d -a \( -name test -o -name tests -o -name idle_test \) \) \
+    -o \
+    \( -type f -a \( -name '*.pyc' -o -name '*.pyo' \) \) \
+    \) -exec rm -rf '{}' +
